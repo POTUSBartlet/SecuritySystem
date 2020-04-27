e87cd2bd3f278b821b280f5e61d3bb98abea77df
parrishmccall <10604064+parrishmccall@users.noreply.github.com>
2020-04-27 14:40:26 -0400

    Update rpi_tf_camera.py

--- rpi_tf_camera.py
+++ rpi_tf_camera.py
@@ -11,15 +11,11 @@ auth_token = "*"
 client = Client(account_sid, auth_token)
 
 
-cap = cv2.VideoCapture("http://192.168.3.24:8081")
+cap = cv2.VideoCapture(0)
 
 # What model to download.
 MODEL_NAME = 'ssdlite_mobilenet_v2_coco_2018_05_09'
 
-MODEL_FILE = MODEL_NAME + '.tar.gz'
-DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/'
-
-# Path to frozen detection graph. This is the actual model that is used for the object detection.
 PATH_TO_CKPT = MODEL_NAME + '/frozen_inference_graph.pb'
 
 # List of the strings that is used to add correct label for each box.
@@ -40,12 +36,6 @@ with detection_graph.as_default():
 label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
 categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
 category_index = label_map_util.create_category_index(categories)
-
-
-def load_image_into_numpy_array(image):
-  (im_width, im_height) = image.size
-  return np.array(image.getdata()).reshape(
-      (im_height, im_width, 3)).astype(np.uint8)
 
 
 with detection_graph.as_default():
